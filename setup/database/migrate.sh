35d5ce637ca003641344d734fdc2a9dcfa8b853c
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-10-19 05:56:10 +0300

    ci: Fixed db migration

--- setup/database/migrate.sh
+++ setup/database/migrate.sh
@@ -12,19 +12,22 @@ DB_USER=postgres
 APP=pc-shop
 PATH_BACKUP=/var/lib/postgresql/backup
 BACKUP=$DB-$(date +%Y-%m-%d).sql
-MIGRATION=/srv/$APP/database.sql
+DB_SRC=/srv/$APP/database.sql
+DB_DESTINATION=/var/lib/postgresql/database.sql
 
+mkdir -p $PATH_BACKUP
+mv $DB_SRC $DB_DESTINATION
 
 echo "Начинаю миграцию базы данных..."
 
-sudo -iu $DB_USER pg_dump $DB > $PATH_BACKUP/$BACKUP
+sudo -iu $DB_USER pg_dump $DB --clean --if-exists &1> $PATH_BACKUP/$BACKUP
 echo "backup $DB-$(date +%Y-%m-%d) dumped"
 
-sudo -iu $DB_USER dropdb $DB
+sudo -iu $DB_USER $(dropdb $DB --if-exists)
 echo "database $DB dropped"
 
 sudo -iu $DB_USER createdb $DB
 echo "database $DB created"
 
-sudo -iu $DB_USER psql -d $DB -f $MIGRATION > /dev/null
+sudo -iu $DB_USER psql -d $DB -f $DB_DESTINATION > /dev/null
 echo "database $DB deploy"
