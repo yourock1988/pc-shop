05d53a50e0a4034f5477addfbd2317c5debbcaa3
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-10-19 05:58:20 +0300

    ci: Using $USER variable

--- setup/init-system.sh
+++ setup/init-system.sh
@@ -12,12 +12,13 @@ GROUP=developers
 
 
 echo "SHELL=/bin/bash" >> /etc/default/useradd
-echo -e '\nexport NVM_DIR="$HOME/.nvm"\n[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> /home/webmaster/.profile
+useradd -m -s /bin/bash $USER
+
+echo -e '\nexport NVM_DIR="$HOME/.nvm"\n[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> /home/$USER/.profile
 echo "SystemMaxUse=50M" >> /etc/systemd/journald.conf
 systemctl restart systemd-journald
 
 
-useradd -m -s /bin/bash $USER
 groupadd $GROUP
 usermod -aG $GROUP $USER
 chgrp $GROUP /srv
@@ -33,9 +34,10 @@ apt install -y postgresql
 apt install -y snapd
 snap install --classic certbot
 
-sudo -iu webmaster bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash'
-sudo -iu webmaster nvm install 22.11.0
-sudo -iu webmaster nvm alias default 22.11.0
+sudo -iu $USER bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash'
+bash /home/$USER/.nvm/nvm.sh
+sudo -iu $USER nvm install 22.14.0
+sudo -iu $USER nvm alias default 22.14.0
 
 ln -s -f /snap/bin/certbot /usr/bin/certbot
 sudo -iu postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres1';"
